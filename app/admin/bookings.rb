c705bfb987bdab8f6904abc8baec4f84ca8f5153
Ryan Mansfield <ryanmansfield@Ryans-MBP.(none)>
2023-04-17 22:33:15 -0600

    Update active-admin config

--- app/admin/bookings.rb
+++ app/admin/bookings.rb
@@ -1,4 +1,7 @@
 ActiveAdmin.register Booking do
+  controller do
+    actions :all, :except => [:edit, :destroy, :new]
+  end
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
